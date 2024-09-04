8cae835309c7eb8c76a4330e505f9964cccac291
GitHub Action <action@github.com>
2024-09-04 05:32:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/hannesa2/generategitchangelog/MyBundle.kt
+++ src/main/kotlin/com/github/hannesa2/generategitchangelog/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hannesa2.generategitchangelog
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hannesa2/generategitchangelog/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/hannesa2/generategitchangelog/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.hannesa2.generategitchangelog.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
